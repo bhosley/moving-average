c49b6b2a47786039a6d660ca2d7dfa5e3179e70e
bhosley <brandonscotthosley@gmail.com>
2020-08-06 10:42:02 -0600

    Update poc.cpp
    Change testing parameters to match new function style

--- poc/poc/poc.cpp
+++ poc/poc/poc.cpp
@@ -5,26 +5,10 @@
 using namespace std;
 
 #include "MovingAverage.h"
-float a[] = { 0, 7, 10, 9, 7.1, 7.5, 7.4, 12, 15, 10, 0, 3, 3.5, 4, 1, 7, 1 };
-
-// Simple moving average plot
-int currentDataPoint = 0;
-float simple_moving_average_previous = 0;
-int NUM_Y_VALUES = 17;
-
-float simple_moving_average() {
-	uint32_t startingValue = currentDataPoint;
-	uint32_t endingValue = (currentDataPoint + 1) % NUM_Y_VALUES;
-	float simple_moving_average_current = simple_moving_average_previous +
-		(a[startingValue]) / NUM_Y_VALUES -
-		(a[endingValue]) / NUM_Y_VALUES;
-
-	currentDataPoint + rand() % 2 + 1;
-	currentDataPoint = currentDataPoint % NUM_Y_VALUES;
-
-	simple_moving_average_previous = simple_moving_average_current;
-	return simple_moving_average_current;
-}
+//float a[] = { 0, 7, 10, 9, 7.1, 7.5, 7.4, 12, 15, 10, 0, 3, 3.5, 4, 1, 7, 1 };
+float a[] = { 1, 1, 1, 1, 1, 1, 1, 1 };
+int currentIndex = 0;
+int lenA = sizeof(a) / sizeof(a[0]);
 
 int main()
 {
@@ -36,26 +20,24 @@ int main()
 	WeightedMovingAverage wma = WeightedMovingAverage();
 	ExponentialMovingAverage ema = ExponentialMovingAverage();
 
-	for (float f : a) {
-		rma.addValue(f);
-		sma.addValue(f);
-		cma.addValue(f);
-		wma.addValue(f);
-		ema.addValue(f);
-	}
-
 	while (true)
 	{
 		cin.get();
+		
+		currentIndex = (currentIndex + rand() % 2 + 1) % lenA;
+		rma.addValue(a[currentIndex]);
+		sma.addValue(a[currentIndex]);
+		cma.addValue(a[currentIndex]);
+		wma.addValue(a[currentIndex]);
+		ema.addValue(a[currentIndex]);
+
 		cout << left
 			<< "MA:  " << setw(6) << ma.getNewValue()
-			<< "RMA: " << setw(10) << rma.getNewValue()
-			<< "SMA: " << setw(10) << sma.getNewValue()
-			<< "CMA: " << setw(10) << cma.getNewValue()
-			<< "WMA: " << setw(10) << wma.getNewValue()
-			<< "EMA: " << setw(10) << ema.getNewValue()
-			//<< "Classic: " << setw(8) << simple_moving_average()
-			;
+			<< "RMA: " << setw(10) << rma.getValue()
+			<< "SMA: " << setw(10) << sma.getValue()
+			<< "CMA: " << setw(10) << cma.getValue()
+			<< "WMA: " << setw(10) << wma.getValue()
+			<< "EMA: " << setw(10) << ema.getValue();
 	}
 	return 0;
 }
