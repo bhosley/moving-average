d8dbc3295068b92e87a7a40e4310dd5d1603be7a
bhosley <brandonscotthosley@gmail.com>
2020-08-06 08:56:48 -0600

    Update MovingAverage.h
    New implementation style of CMA

--- poc/poc/MovingAverage.h
+++ poc/poc/MovingAverage.h
@@ -5,14 +5,17 @@ class MovingAverage
 {
 public:
 	MovingAverage();
+	MovingAverage(float f);
 	~MovingAverage();
 	float getValue();
 	float getNewValue();
 	virtual void newValue();
 	virtual float addValue(float f);
 	void changeValue(int i, float f);
+	int x = 0;
 protected:
 	void init(float a[], int initialValue);
+	void init(float f);
 	float currentValue;
 	int y;
 	std::vector<float> yVals;
@@ -35,24 +38,26 @@ private:
 class CumulativeMovingAverage : public MovingAverage
 {
 public:
-	void newValue() override;
+	CumulativeMovingAverage();
+	CumulativeMovingAverage(float f);
+	float addValue(float f) override;
 private:
-	int x = 1;
 };
 class WeightedMovingAverage : public MovingAverage
 {
 public:
+	WeightedMovingAverage(float f);
 	float addValue(float f) override;
 private:
-	int x = 1;
 };
 class ExponentialMovingAverage : public MovingAverage
 {
 public:
+	ExponentialMovingAverage();
+	ExponentialMovingAverage(float f);
 	float addValue(float f) override;
 	float a = 0.9f;
 private:
 	float currentDenominator = 0.0f;
 	float currentNumerator = 0.0f;
-	int x = 1;
 };
