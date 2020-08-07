dcaeb94a777d7cdcbee6fdc7faf48f5a48b3394f
bhosley <brandonscotthosley@gmail.com>
2020-08-07 00:39:39 -0600

    Update poc.cpp
    Function call corrections,
    simplify instantiations

--- poc/poc/poc.cpp
+++ poc/poc/poc.cpp
@@ -5,8 +5,8 @@
 using namespace std;
 
 #include "MovingAverage.h"
-//float a[] = { 0, 7, 10, 9, 7.1, 7.5, 7.4, 12, 15, 10, 0, 3, 3.5, 4, 1, 7, 1 };
-float a[] = { 1, 1, 1, 1, 1, 1, 1, 1 };
+float a[] = { 0, 7, 10, 9, 7.1, 7.5, 7.4, 12, 15, 10, 0, 3, 3.5, 4, 1, 7, 1 };
+//float a[] = { 1, 1, 1, 1, 1, 1, 1, 1 };
 int currentIndex = 0;
 int lenA = sizeof(a) / sizeof(a[0]);
 
@@ -14,17 +14,18 @@ int main()
 {
 	cout << "Please press enter for new MA Test.";
 	MovingAverage ma;
-	RandomMovingAverage rma = RandomMovingAverage();
-	SimpleMovingAverage sma = SimpleMovingAverage();
-	CumulativeMovingAverage cma = CumulativeMovingAverage();
-	WeightedMovingAverage wma = WeightedMovingAverage();
-	ExponentialMovingAverage ema = ExponentialMovingAverage();
+	RandomMovingAverage rma;
+	SimpleMovingAverage sma;
+	CumulativeMovingAverage cma;
+	WeightedMovingAverage wma;
+	ExponentialMovingAverage ema;
 
 	while (true)
 	{
 		cin.get();
 		
 		currentIndex = (currentIndex + rand() % 2 + 1) % lenA;
+		ma.addValue(a[currentIndex]);
 		rma.addValue(a[currentIndex]);
 		sma.addValue(a[currentIndex]);
 		cma.addValue(a[currentIndex]);
@@ -32,7 +33,7 @@ int main()
 		ema.addValue(a[currentIndex]);
 
 		cout << left
-			<< "MA:  " << setw(6) << ma.getNewValue()
+			<< "MA:  " << setw(6) << ma.getValue()
 			<< "RMA: " << setw(10) << rma.getValue()
 			<< "SMA: " << setw(10) << sma.getValue()
 			<< "CMA: " << setw(10) << cma.getValue()
